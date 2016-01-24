43a4f5b54391df9f11b4505c0c2631efd225a23b
Nishant Rodrigues <nishantjr@gmail.com>
2016-01-24 18:45:01 +0900

    gulp: Add task for functional tests.
    The functional tests depend on the unit tests to prevent interleaving the output
    of unit-test and functional-test tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 'use strict'
 
-const gulp = require('gulp')
-const exclude_gitignore = require('gulp-exclude-gitignore')
+const
+    gulp = require('gulp'),
+    exclude_gitignore = require('gulp-exclude-gitignore'),
+    tape = require('gulp-tape'),
+    faucet = require('faucet')
 
-gulp.task('test', ['unit-test'])
+gulp.task('test', ['unit-test', 'functional-test'])
 
 gulp.task('unit-test', () => {
-    const
-        tape = require('gulp-tape'),
-        faucet = require('faucet')
     return gulp
         .src('**/*.jt')
         .pipe(exclude_gitignore())
+        .pipe(tape({ reporter: faucet() }))
+})
+
+gulp.task('functional-test', ['unit-test'], () => {
+    return gulp
+        .src('t/t[0-9]*.js')
         .pipe(tape({ reporter: faucet() }))
 })
