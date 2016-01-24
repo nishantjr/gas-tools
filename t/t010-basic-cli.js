c368678ab451da86470610cdc3ef385f77ab765f
Nishant Rodrigues <nishantjr@gmail.com>
2016-01-24 19:19:21 +0900

    gapps: Fail when a bad command is passed on the CLI.

--- t/t010-basic-cli.js
+++ t/t010-basic-cli.js
@@ -2,11 +2,23 @@
 const tlib = require('./tlib')(module)
 
 // Smoke test for the command line program.
-tlib.test('version', (t) => {
+tlib.test('help', (t) => {
     const
         expected = require('../package.json').version + '\n',
         cmd = tlib.spawn(t, './bin/gapps -V')
     cmd.succeeds()
     cmd.stdout.match(expected)
+    cmd.end()
+})
+
+tlib.test('no command fails', (t) => {
+    const cmd = tlib.spawn(t, './bin/gapps')
+    cmd.fails()
+    cmd.end()
+})
+
+tlib.test('bad commands fail', (t) => {
+    const cmd = tlib.spawn(t, './bin/gapps rubbish')
+    cmd.fails()
     cmd.end()
 })
