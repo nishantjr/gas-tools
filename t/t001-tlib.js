b8620f0291579cab2b57e553609fadd744c83572
Curt J. Sampson <cjs@cynic.net>
2016-01-25 00:09:28 +0900

    t/tlib: extract testName function.

--- t/t001-tlib.js
+++ t/t001-tlib.js
@@ -1,6 +1,11 @@
 'use strict'
 const tlib = require('./tlib')(module)
 
+tlib.test('testName', (t) => {
+    t.equal(tlib.testName(), 't001')
+    t.end()
+})
+
 tlib.test('readTestData', (t) => {
     t.equal('A file containing expected output.\n',
         tlib.readTestData(tlib.testData('expected')))
--- t/tlib.js
+++ t/tlib.js
@@ -3,11 +3,13 @@ const
     path        = require('path'),
     _           = require('lodash')
 
+function testName(test_module) {
+    return path.basename(test_module.filename, '.js').split('-')[0]
+}
+
 function testData(test_module, p) {
-    const
-        dir = path.dirname(test_module.filename),
-        subdir = path.basename(test_module.filename, '.js').split('-')[0]
-    return path.join(dir, subdir, p)
+    return path.join(
+        path.dirname(test_module.filename), testName(test_module), p)
 }
 
 function readTestData(p) {
@@ -17,6 +19,7 @@ function readTestData(p) {
 module.exports = (test_module) => { return {
     test:               require('tape'),
     spawn:              require('tape-spawn'),
+    testName:           _.partial(testName, test_module),
     testData:           _.partial(testData, test_module),
     readTestData:       readTestData,
 }}
