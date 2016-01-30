7f694f472d39ba8da4a934f1128d220e17bfa700
Nishant Rodrigues <nishantjr@gmail.com>
2016-01-30 23:07:07 +0900

    t/t020-init.js: Verify contents of downloaded script.

--- t/t020-init.js
+++ t/t020-init.js
@@ -1,14 +1,24 @@
 'use strict'
-const tlib = require('./tlib')(module)
+const tlib = require('./tlib')(module),
+      bluebird = require('bluebird'),
+      fs   = bluebird.promisifyAll(require('fs'))
 
-tlib.test('pwd', (t) => {
+tlib.test('gas init', (t) => {
     // GDrive publicly readable test file made available by <cjs@cynic.net>.
     // (In the long run we should probably create our own test files.)
     const docId = '1CodFWMEXI-5MfzNniEe8Uw8pSi82Iz0uU_jdbUvs2YpAIVmNqb-aH-Xg'
 
     const scratchDir = tlib.cleanScratchDir()
-    const cmd = tlib.spawn(t, '../../../bin/gas init ' + docId,
-        { cwd: scratchDir } )
+    const cmd = bluebird.promisifyAll(
+        tlib.spawn(t, '../../../bin/gas init ' + docId,
+        { cwd: scratchDir }))
+
     cmd.stdout.match('')
-    cmd.end()
+
+    const firstLine =
+        '// GAScode.gs from node-google-apps-script Public Test GAS Project'
+    cmd.endAsync()
+       .then(() => fs.readFileAsync(tlib.scratchFile('src/GAScode.js')))
+       .then((buffer) =>
+            t.equal(buffer.toString('utf8').split('\n')[0], firstLine))
 })
