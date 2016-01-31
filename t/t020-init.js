6575ac08372a4caaa3f5b87409978470edaaeb30
Nishant Rodrigues <nishantjr@gmail.com>
2016-01-31 21:47:25 +0900

    t020-init: No need to use promises, since tape runs tests synchronously.
    Tape (and at least some things that use it, such as tape-spawn) ensures
    that tests run one-at-a-time and in order. Thus, we don't need to use
    promises to enforce this; we can just put the tests in a particular file
    in dependency order.
    
    This actually isn't terribly pretty or comfortable, but it's the best
    we've got for now.

--- t/t020-init.js
+++ t/t020-init.js
@@ -1,33 +1,25 @@
 'use strict'
 const tlib = require('./tlib')(module),
-      bluebird = require('bluebird'),
-      fs   = bluebird.promisifyAll(require('fs'))
+      fs   = require('fs')
 
-tlib.test('gas init', (t) => {
+const docId = '1CodFWMEXI-5MfzNniEe8Uw8pSi82Iz0uU_jdbUvs2YpAIVmNqb-aH-Xg',
+      scratchDir = tlib.cleanScratchDir()
+
+tlib.test('gas init', function(t) {
     // GDrive publicly readable test file made available by <cjs@cynic.net>.
     // (In the long run we should probably create our own test files.)
-    const docId = '1CodFWMEXI-5MfzNniEe8Uw8pSi82Iz0uU_jdbUvs2YpAIVmNqb-aH-Xg'
 
-    const scratchDir = tlib.cleanScratchDir()
-    const cmd = bluebird.promisifyAll(
-        tlib.spawn(t, '../../../bin/gas init ' + docId,
-        { cwd: scratchDir, end: false }))
+    const cmd = tlib.spawn(t, '../../../bin/gas init ' + docId,
+        { cwd: scratchDir })
     cmd.stdout.match('')
+    cmd.end()
+})
 
+tlib.test('check init content', t => {
     const firstLine =
         '// GAScode.gs from node-google-apps-script Public Test GAS Project'
-    cmd.endAsync()
-       .then(() => fs.readFileAsync(tlib.scratchFile('src/GAScode.js')))
-       .then((buffer) => buffer.toString('utf8').split('\n')[0] )
-       .then((actualFirstLine) => t.equal(firstLine, actualFirstLine))
-       .then(() => {
-            const cmd = bluebird.promisifyAll(
-                tlib.spawn(t, '../../../bin/gas init --overwrite' + docId,
-                { cwd: scratchDir, end: false }))
-            cmd.stdout.match('')
-            return cmd.endAsync()
-        })
-       .then(t.end)
-
-
+    const buffer = fs.readFileSync(tlib.scratchFile('src/GAScode.js'))
+    const actualFirstLine = buffer.toString('utf8').split('\n')[0]
+    t.equal(firstLine, actualFirstLine)
+    t.end()
 })
