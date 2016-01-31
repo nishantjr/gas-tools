87d4bd1e10f0ccd3b3a9b8ca7f8a339151b512ef
Nishant Rodrigues <nishantjr@gmail.com>
2016-01-31 22:48:41 +0900

    t020-init: Move comment regarding test project to where projectId is declared.

--- t/t020-init.js
+++ t/t020-init.js
@@ -2,13 +2,13 @@
 const tlib = require('./tlib')(module),
       fs   = require('fs')
 
-const docId = '1CodFWMEXI-5MfzNniEe8Uw8pSi82Iz0uU_jdbUvs2YpAIVmNqb-aH-Xg',
-      scratchDir = tlib.cleanScratchDir()
+// GDrive publicly readable test file made available by <cjs@cynic.net>.
+// (In the long run we should probably create our own test files.)
+const docId = '1CodFWMEXI-5MfzNniEe8Uw8pSi82Iz0uU_jdbUvs2YpAIVmNqb-aH-Xg'
+
+const scratchDir = tlib.cleanScratchDir()
 
 tlib.test('gas init', function(t) {
-    // GDrive publicly readable test file made available by <cjs@cynic.net>.
-    // (In the long run we should probably create our own test files.)
-
     const cmd = tlib.spawn(t, '../../../bin/gas init ' + docId,
         { cwd: scratchDir })
     cmd.stdout.match('')
